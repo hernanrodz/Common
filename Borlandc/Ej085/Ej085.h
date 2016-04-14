1a99dc07c1914ca95a609d4452a03a23f1996cea
hernanrodz <hernan.rodriguez.ext@gmail.com>
2016-04-14 20:21:49 -0300

    Ej085.cpp: Botones relacionados, SpeedButtons.

---
+++ Borlandc/Ej085/Ej085.h
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+
+#ifndef Ej085H
+#define Ej085H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <Buttons.hpp>
+#include <ExtCtrls.hpp>
+#include <ComCtrls.hpp>
+#include <ToolWin.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TSpeedButton *SpeedButton1;
+        TSpeedButton *SpeedButton2;
+        TSpeedButton *SpeedButton3;
+        TSpeedButton *SpeedButton4;
+        TSpeedButton *SpeedButton5;
+        TSpeedButton *SpeedButton6;
+        TToolBar *ToolBar1;
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
